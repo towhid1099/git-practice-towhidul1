f07fa53632346163deebfed7deabe63bb54b2532
Md Towhidul Islam <towhid1099@gmail.com>
2026-05-01 13:38:52 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+import utils
 
 print("Name: Md Towhidul Islam")
 print(f"Date: {date.today()}")
+
+print("10 + 5 =", utils.add(10, 5))
+print("10 - 5 =", utils.subtract(10, 5))
